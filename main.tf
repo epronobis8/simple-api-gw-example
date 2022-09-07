3240abc0b2a7a41b5414c29bfededdae4a3d0626
Erin Pronobis <erinpronobis@Erins-MacBook-Air.local>
2022-09-07 12:23:33 -0400

    updates to iac

--- main.tf
+++ main.tf
@@ -61,6 +61,8 @@ resource "aws_api_gateway_method" "product" {
   resource_id   = aws_api_gateway_resource.product.id
   http_method   = each.key
   authorization = "NONE"
+  # added 9/7
+  api_key_required = true
 }
 
 resource "aws_api_gateway_integration" "product" {
@@ -349,4 +351,7 @@ resource "aws_api_gateway_method_settings" "api-logs" {
   }
 }
 
-
+# added 9/7 - api key
+resource "aws_api_gateway_api_key" "MyDemoApiKey" {
+  name = "demo-api-gateway-product-key"
+}
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,13 @@
-
 output "api-gateway-url" {
   value = "${aws_api_gateway_stage.example.invoke_url}"
 }
 
 output "product-url" {
-  value = "${aws_api_gateway_stage.example.invoke_url}:/:${aws_api_gateway_resource.product.path_part}"
+  value = "${aws_api_gateway_stage.example.invoke_url}:/${aws_api_gateway_resource.product.path_part}"
 }
 output "health-url" {
-  value = "${aws_api_gateway_stage.example.invoke_url}:${var.stage-name}:${aws_api_gateway_resource.health-api.path_part}"
+  value = "${aws_api_gateway_stage.example.invoke_url}:/${aws_api_gateway_resource.health-api.path_part}"
 }
 output "products-url" {
-  value = "${aws_api_gateway_stage.example.invoke_url}:${var.stage-name}:${aws_api_gateway_resource.products-api.path_part}"
+  value = "${aws_api_gateway_stage.example.invoke_url}:/${aws_api_gateway_resource.products-api.path_part}"
 }
